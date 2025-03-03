52f6314f9f9ac03381adfbd7a29adb7a1a85f7cd
Molkang <mitier26@gmail.com>
2025-03-03 21:58:27 +0900

    적 움직임

--- Space Shooter/Assets/02.Scripts/EnemyController.cs
+++ Space Shooter/Assets/02.Scripts/EnemyController.cs
@@ -5,9 +5,29 @@ public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
 
+    public Vector2 startDirection;
+    
+    public bool shouldChangeDirection;
+    public float changeDirectionXPoint;
+    public Vector2 changeDirection;
+
     private void Update()
     {
-        transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+        if (!shouldChangeDirection)
+        {
+            transform.position += new Vector3(startDirection.x * moveSpeed * Time.deltaTime, startDirection.y * moveSpeed * Time.deltaTime, 0);
+        }
+        else
+        {
+            if (transform.position.x > changeDirectionXPoint)
+            {
+                transform.position += new Vector3(startDirection.x * moveSpeed * Time.deltaTime, startDirection.y * moveSpeed * Time.deltaTime, 0);
+            }
+            else
+            {
+                transform.position += new Vector3(changeDirection.x * moveSpeed * Time.deltaTime, changeDirection.y * moveSpeed * Time.deltaTime, 0);
+            }
+        }
     }
 
     private void OnBecameInvisible()
