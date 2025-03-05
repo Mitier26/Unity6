19b85ec8a14608e08a1682c6aae4b38d596d0cb4
Molkang <mitier26@gmail.com>
2025-03-06 00:53:26 +0900

    웨이브 메이저

---
+++ Space Shooter/Assets/02.Scripts/WaveManager.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance;
+
+    public WaveObject[] waves;
+
+    public int currentWave;
+    
+    public float timeToNextWave;
+
+    public bool canSpawnWaves = true;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        timeToNextWave = waves[0].timeToSpawn;
+    }
+
+    private void Update()
+    {
+        if (canSpawnWaves)
+        {
+            timeToNextWave -= Time.deltaTime;
+
+            if (timeToNextWave <= 0)
+            {
+                Instantiate(waves[currentWave].theWave, transform.position, transform.rotation);
+
+                if (currentWave < waves.Length - 1)
+                {
+                    currentWave++;
+            
+                    timeToNextWave = waves[currentWave].timeToSpawn;
+                }
+                else
+                {
+                    canSpawnWaves = false;
+                }
+            }
+        }
+    }
+}
+
+[Serializable]
+public class WaveObject
+{
+    public float timeToSpawn;
+    public EnemyWave theWave;
+}
