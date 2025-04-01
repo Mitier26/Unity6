32268c657160a16ae2ac5ea2f0e535bca95fd175
Molkang <mitier26@gmail.com>
2025-04-01 11:27:15 +0900

    보스 공격

--- Space Shooter/Assets/02.Scripts/BossManager.cs
+++ Space Shooter/Assets/02.Scripts/BossManager.cs
@@ -7,7 +7,8 @@ public class BossManager : MonoBehaviour
 
     public string bossName;
     public int currentHealth = 100;
-    
+
+    public BattleShot[] shotsToFire;
     
     private void Awake()
     {
@@ -20,6 +21,22 @@ public class BossManager : MonoBehaviour
         UIManager.instance.bossSlider.maxValue = currentHealth;
         UIManager.instance.bossSlider.value = currentHealth;
         UIManager.instance.bossSlider.gameObject.SetActive(true);
+        
+        MusicController.instance.PlayBoos();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < shotsToFire.Length; i++)
+        {
+            shotsToFire[i].shotCounter -= Time.deltaTime;
+
+            if (shotsToFire[i].shotCounter <= 0)
+            {
+                shotsToFire[i].shotCounter = shotsToFire[i].timeBetweenShots;
+                Instantiate(shotsToFire[i].theShot, shotsToFire[i].firePoint.position, shotsToFire[i].firePoint.rotation);
+            }
+        }
     }
 
     public void HurtBoss()
@@ -33,4 +50,13 @@ public class BossManager : MonoBehaviour
             UIManager.instance.bossSlider.gameObject.SetActive(false);
         }
     }
+}
+
+[System.Serializable]
+public class BattleShot
+{
+    public GameObject theShot;
+    public float timeBetweenShots;
+    public float shotCounter;
+    public Transform firePoint;
 }
