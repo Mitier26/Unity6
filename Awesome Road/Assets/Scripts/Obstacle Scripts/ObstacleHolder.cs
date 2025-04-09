433f9380bb829c5e6481d060e4ed9ddec9951000
Molkang <mitier26@gmail.com>
2025-04-09 16:55:22 +0900

    장애물 설정

---
+++ Awesome Road/Assets/Scripts/Obstacle Scripts/ObstacleHolder.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObstacleHolder : MonoBehaviour
+{
+    public GameObject[] childs;
+
+    public float limitAxisX;
+
+    public Vector3 firstPos, secondPos;
+
+
+    private void Update()
+    {
+        transform.position += new Vector3(-GameplayController.instance.moveSpeed * Time.deltaTime, 0f, 0f);
+
+        if (transform.localPosition.x <= limitAxisX)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        for(int i = 0; i < childs.Length; i++)
+        {
+            childs[i].SetActive(true);
+        }
+
+        if (Random.value <= 0.5f)
+        {
+            transform.localPosition = firstPos;
+        }
+        else
+        {
+            transform.localPosition = secondPos;
+        }
+    }
+}
