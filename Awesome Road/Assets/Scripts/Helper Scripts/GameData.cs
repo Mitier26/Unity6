60cb96ffc5a1f1c95401ec5b87385178f7efc00d
Molkang <mitier26@gmail.com>
2025-04-10 01:08:34 +0900

    데이터 저장

---
+++ Awesome Road/Assets/Scripts/Helper Scripts/GameData.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class GameData
+{
+   private int star_Score;
+   private int score_Count;
+
+   private bool[] heroes;
+
+   private int selected_Index;
+
+   public int StarScore
+   {
+      get
+      {
+         return star_Score;
+      }
+      set
+      {
+         star_Score = value;
+      }
+   }
+   
+   public int ScoreCount
+   {
+      get
+      {
+         return score_Count;
+      }
+      set
+      {
+         score_Count = value;
+      }
+   }
+   
+   public bool[] Heroes
+   {
+      get
+      {
+         return heroes;
+      }
+      set
+      {
+         heroes = value;
+      }
+   }
+   
+   public int SelectedIndex
+   {
+      get
+      {
+         return selected_Index;
+      }
+      set
+      {
+         selected_Index = value;
+      }
+   }
+
+}
