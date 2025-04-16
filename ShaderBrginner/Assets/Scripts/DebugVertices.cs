e2c8b0775c56294bfa731e4801de94bc6c40c7d1
Molkang <mitier26@gmail.com>
2025-04-16 10:26:21 +0900

    Position

--- ShaderBrginner/Assets/Scripts/DebugVertices.cs
+++ ShaderBrginner/Assets/Scripts/DebugVertices.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class DebugVertices : MonoBehaviour {
@@ -15,12 +16,15 @@ public class DebugVertices : MonoBehaviour {
 			mesh = GetComponent<MeshFilter>().sharedMesh;
 			vertices = mesh.vertices;
 		}
+
 		foreach (Vector3 v in vertices)
-			{
-				string vert = "v: " + v.ToString();
-				string worldVert = " Wv: " + (transform.position + v).ToString();
-				 UnityEditor.Handles.Label(transform.position + v, vert + worldVert);
-			}
-           
+		{
+			Vector3 viewVertValue = SceneView.GetAllSceneCameras()[0].WorldToViewportPoint(transform.position + v);
+			string vert = "v: " + v.ToString();
+			string worldVert = " Wv: " + (transform.position + v).ToString();
+			string viewVert = " Vv: " + viewVertValue.ToString();
+			UnityEditor.Handles.Label(transform.position + v, vert + worldVert + viewVert);
+		}
+
 	}
 }
