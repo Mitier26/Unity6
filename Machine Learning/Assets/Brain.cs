2a978c905fa0837ce83fe55be7c240498f496503
Molkang <mitier26@gmail.com>
2025-05-18 23:24:26 +0900

    이동 브레인

--- Machine Learning/Assets/Brain.cs
+++ Machine Learning/Assets/Brain.cs
@@ -1,16 +1,53 @@
+using System.Net;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
+[RequireComponent(typeof(ThirdPersonCharacter))]
 public class Brain : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int dnaLength = 1;
+    public float timeAlive;
+    public DNA dna;
+
+    private ThirdPersonCharacter m_Character;
+    private Vector3 m_Move;
+    private bool m_Jump;
+    bool alive = true;
+
+    void OCollisionEnter(Collision obj)
     {
-        
+        if (obj.gameObject.tag == "dead")
+        {
+            alive = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init()
     {
-        
+        dna = new DNA(dnaLength, 6);
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        timeAlive = 0;
+        alive = true;
+    }
+
+    void FixedUpdate()
+    {
+        float h = 0;
+        float v = 0;
+        bool crouch = false;
+        if (dna.GetGene(0) == 0) v = 1;
+        else if (dna.GetGene(0) == 1) v = -1;
+        else if (dna.GetGene(0) == 2) h = -1;
+        else if (dna.GetGene(0) == 3) h = 1;
+        else if (dna.GetGene(0) == 4) m_Jump = true;
+        else if (dna.GetGene(0) == 5) crouch = true;
+
+        m_Move = v * Vector3.forward + h * Vector3.right;
+        m_Character.Move(m_Move, crouch, m_Jump);
+        m_Jump = false;
+        if (alive)
+        {
+            timeAlive += Time.deltaTime;
+        }
     }
 }
