3afa3294e814424ac2cc9e8cdca9fca649a3de48
Molkang <mitier26@gmail.com>
2025-07-02 11:15:02 +0900

    코인

---
+++ 2D Endless Runnder/Assets/Scripts/Coin.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.instance.coins++;
+            Destroy(gameObject);
+        }
+    }
+
+}
